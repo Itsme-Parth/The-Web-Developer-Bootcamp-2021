d8264a55960509db9cfa24b951e2773ef278961b
Parth Chadha <parthc2001@gmail.com>
2021-07-07 12:07:10 +0530

    Array Methods Added

---
+++ JavaScript/Arrays/Push, Pop & Shift, Unshift/app.js
@@ -0,0 +1,20 @@
+let arr = ["red", "blue", "green"];
+arr[0] = "pink";
+
+/*Stack simulation methods*/
+
+//Push Method allows you to enter at the end
+arr.push("purple");
+//Pop method allows you to delete the element from the end
+arr.pop();
+
+/*Queue simulation methods*/
+
+//Shift method allows you to delete from the front
+arr.shift;
+//Unshift method allows you to add from the front
+
+arr.unshift("black");
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
