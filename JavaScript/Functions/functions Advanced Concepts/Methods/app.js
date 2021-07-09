79d08aaa845b7b3f40fd162c0276f157f5f4ff33
Parth Chadha <parthc2001@gmail.com>
2021-07-09 23:45:43 +0530

    Methods Demo Added

--- JavaScript/Functions/functions Advanced Concepts/Methods/app.js
+++ JavaScript/Functions/functions Advanced Concepts/Methods/app.js
@@ -1,21 +1,30 @@
 const myMath = {
-    PI: 3.14159,
-    square(num) {
-        return num * num;
-    },
-    cube(num) {
-        return num ** 3;
-    }
-}
+  PI: 3.14159,
+  square(num) {
+    return num * num;
+  },
+  cube(num) {
+    return num ** 3;
+  },
+};
 
 const cat = {
-    name: 'Blue Steele',
-    color: 'grey',
-    breed: 'scottish fold',
-    meow() {
-        console.log("THIS IS:", this)
-        console.log(`${this.name} says MEOWWWW`);
-    }
-}
+  name: "Blue Steele",
+  color: "grey",
+  breed: "scottish fold",
+  meow() {
+    console.log("THIS IS:", this);
+    console.log(`${this.name} says MEOWWWW`);
+  },
+};
 
 const meow2 = cat.meow;
+/* This keyword Exercise */
+let hen = {
+  name: "Helen",
+  eggCount: 0,
+  layAnEgg() {
+    this.eggCount++;
+    return "EGG";
+  },
+};
