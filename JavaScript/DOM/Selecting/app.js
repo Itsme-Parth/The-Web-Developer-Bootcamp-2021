2889441ebe2fd0c7802dd60c03064241e9cd3f00
Parth Chadha <parthc2001@gmail.com>
2021-07-12 18:17:50 +0530

    Selecting DOM Demo Added

---
+++ JavaScript/DOM/Selecting/app.js
@@ -0,0 +1,27 @@
+const allImages = document.getElementsByTagName("img");
+
+for (let img of allImages) {
+  img.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+}
+
+const squareImages = document.getElementsByClassName("square");
+
+for (let img of squareImages) {
+  img.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+}
+
+const links = document.querySelectorAll("p a");
+
+for (let link of links) {
+  console.log(link.href);
+}
+
+/* getElementById Exercise */
+let image = document.getElementById("unicorn");
+let heading = document.getElementById("mainheading");
+
+/*querySelectors Exercise */
+let doneTodos = document.querySelectorAll("li.done");
+let checkbox = document.querySelector("input[type='checkbox']");
