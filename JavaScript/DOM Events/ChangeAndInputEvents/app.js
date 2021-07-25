c56d9d28db7c8b144b336e2a63e651c6246cb8b5
Parth Chadha <parthc2001@gmail.com>
2021-07-25 16:21:08 +0530

    ChangeAndInput Events Demo Added

--- JavaScript/DOM Events/ChangeAndInputEvents/app.js
+++ JavaScript/DOM Events/ChangeAndInputEvents/app.js
@@ -1,10 +1,21 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
 // input.addEventListener('change', function (e) {
 //     console.log("CASKDJASKJHD")
-// })
+// }) // works only when you change the input and press away from the field
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+input.addEventListener("input", function (e) {
+  h1.innerText = input.value;
+});
+
+/* Exercise */
+let header = document.querySelector("h1");
+let input = document.querySelector("input");
+
+input.addEventListener("input", () => {
+  header.innerHTML = `Welcome, ${input.value}`;
+  if (input.value === "") {
+    header.innerHTML = "Enter Your Username";
+  }
+});
