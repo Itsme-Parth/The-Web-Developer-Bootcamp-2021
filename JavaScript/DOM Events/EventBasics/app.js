984f14ce20098e7c77be4b24bdfca5534be9616b
Parth Chadha <parthc2001@gmail.com>
2021-07-26 17:37:30 +0530

    Event Basics Added

--- JavaScript/DOM Events/EventBasics/app.js
+++ JavaScript/DOM Events/EventBasics/app.js
@@ -1,39 +1,51 @@
-const btn = document.querySelector('#v2');
+const btn = document.querySelector("#v2");
 
 btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
-}
+  console.log("YOU CLICKED ME!");
+  console.log("I HOPE IT WORKED!!");
+};
 
 function scream() {
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
+  console.log("AAAAAHHHHH");
+  console.log("STOP TOUCHING ME!");
 }
 
 btn.onmouseenter = scream;
 
+document.querySelector("h1").onclick = () => {
+  alert("you clicked the h1!");
+};
 
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
-}
-
-
-const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert("CLICKED!");
-})
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function () {
+  alert("CLICKED!");
+});
 
 function twist() {
-    console.log("TWIST!")
+  console.log("TWIST!");
 }
 function shout() {
-    console.log("SHOUT!")
+  console.log("SHOUT!");
 }
 
-const tasButton = document.querySelector('#tas');
+const tasButton = document.querySelector("#tas");
 
 // tasButton.onclick = twist;
 // tasButton.onclick = shout;
 
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+tasButton.addEventListener("click", twist);
+tasButton.addEventListener("click", shout);
+
+const btn1 = document.querySelector("#hello");
+const btn2 = document.querySelector("#goodbye");
+
+const func1 = () => {
+  console.log("hello");
+};
+
+const func2 = () => {
+  console.log("goodbye");
+};
+
+btn1.addEventListener("click", func1);
+btn2.addEventListener("click", func2);
